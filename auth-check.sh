8d4ef91a998542fdb6b60dfec2da6187bcce2a43
Nishant Joshi <nishantjo.12@gmail.com>
2020-02-28 22:33:40 +0530

    DEBUG signal fix

--- auth-check.sh
+++ auth-check.sh
@@ -13,6 +13,6 @@ fi
 if [[ $(echo $DEVPATH | su - {} -c 'read e;export DISPLAY=:0.0;zenity --question --text "Do you wish to connect $(cat /sys$e/product)?" --width 500;echo $?') -eq 0 ]]; then
 	if [[ $(cat /root/USBAuth/password) == $(su - nishant -c "export DISPLAY=:0.0;zenity --password") ]]; then
 		echo 1 > /sys$DEVPATH/authorized
-		# su - nishant -c "export DISPLAY=:0.0;zenity --warning" ## DEBUG SIGNALS
+		# su - <USER> -c "export DISPLAY=:0.0;zenity --warning" ## DEBUG SIGNALS
 	fi
 fi
